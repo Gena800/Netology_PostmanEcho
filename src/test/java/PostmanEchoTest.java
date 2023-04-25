eb625833a48563bc0c55f23a21c836a0be77f64d
Gennadii Lezhnev <genlezhnev@gmail.com>
2023-04-25 18:34:26 +0300

    error fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -11,7 +11,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Вф")
+                .body("Втф")
                 .when()
                 .post("/post")
                 .then()
